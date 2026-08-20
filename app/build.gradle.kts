6477e23a288ae4659a0e5f3b4f6e84877693b98f
Johanna <johakovi@proton.me>
2026-08-20 21:26:57 +0300

    Update versionName to 'Memories M8.1'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 40
-        versionName = "Memories M8"
+        versionName = "Memories M8.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
